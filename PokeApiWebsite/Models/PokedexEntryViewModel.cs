c756b8c0b5a2193cb78179ba234823527c1d4535
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2020-04-28 21:36:15 -0700

    Create Pokedex page

--- PokeApiWebsite/Models/PokedexEntryViewModel.cs
+++ PokeApiWebsite/Models/PokedexEntryViewModel.cs
@@ -8,7 +8,7 @@ namespace PokeApiWebsite.Models
     /// <summary>
     /// Information for a single Pokemon Pokedex Entry
     /// </summary>
-    public class PokemonPokeDexEntryViewModel
+    public class PokedexEntryViewModel
     {
         public string PokedexImageUrl { get; set; }
 
