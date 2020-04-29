96655657a18169adb2a2f53e51b7af7ec7a50565
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2020-04-28 21:56:21 -0700

    Issue#3 pokedex website (#6)
    * Added PokeAPI MVC Website
    
    * Create Pokedex View Model
    
    * Added image to Pokedex ViewModel
    
    * Create Pokedex page
    
    * Refactor Pokemon property names
    
    * Refactoring and cleaning up page

---
+++ PokeApiWebsite/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeApiCore;
+using PokeApiWebsite.Models;
+
+namespace PokeApiWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            PokeApiClient myClient = new PokeApiClient();
+            Pokemon result = await myClient.GetPokemonById(1);
+
+            List<string> resultMoves = new List<string>();
+            foreach(Move currMove in result.moves)
+            {
+                resultMoves.Add(currMove.move.name);
+            }
+
+            resultMoves.Sort();
+
+            var entry = new PokedexEntryViewModel()
+            {
+                Id = result.Id,
+                Name = result.Name,
+                Height = result.Height.ToString(),
+                Weight = result.Weight.ToString(),
+                PokedexImageUrl = result.Sprites.FrontDefault,
+                MoveList = resultMoves
+            };
+            entry.Name = entry.Name.FirstCharToUpper();
+
+            return View(entry);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
